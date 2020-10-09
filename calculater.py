10f8cf6cd2062a99fd827c37fe295d361ec40331
YusunPark <solarpy@hanyang.ac.kr>
2020-10-09 16:08:57 +0900

    Create calculater.py

---
+++ calculater.py
@@ -0,0 +1,13 @@
+def Calculater(a, b):
+    a = int(a)
+    b = int(b)
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+    print(a%b)
+    print(a**b)
+    print(-a)
+    
+    return None
