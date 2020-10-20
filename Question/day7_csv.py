47a4aab24eee66e8a8f9166af69834388814187b
YusunPark <solarpy@hanyang.ac.kr>
2020-10-20 11:37:51 +0900

    Study saving csv file

--- Question/day7_csv.py
+++ Question/day7_csv.py
@@ -13,38 +13,30 @@ companies_soup = BeautifulSoup(companies_request.text, "html.parser")
 goods = companies_soup.select("#MainSuperBrand > ul > li > a.goodsBox-info")
 
 for good in goods:
-  # company = {
-  #   'name' : good.select_one("span.company").text,
-  #   'link' : good['href']
-  # }
-  name = good.select_one("span.company").text,
+  name = good.select_one("span.logo > img")['alt'],
   link = good['href']
-
+  print(name)
   jobs_request = requests.get(link)
   jobs_soup = BeautifulSoup(jobs_request.text, "html.parser")
 
-  job_count = jobs_soup.select_one("#NormalInfo > p.jobCount > strong").text
-  print(job_count)
+  job_count = jobs_soup.select_one("#NormalInfo > p > strong").text
+  
 
-  if job_count != 0:  
+  if job_count != '0':  
     table = jobs_soup.select("#NormalInfo > table > tbody > tr")
     summary_table = jobs_soup.select("#NormalInfo > table > tbody > tr.summaryView")
     jobs = [x for x in table if x not in summary_table]
 
-    f = open(f'{name}.csv', 'w', encoding='utf-8')
-    print(job_count)
+    f = open(f'{name[0]}.csv', 'w', encoding='utf-8')
 
     for job in jobs:
-      location = job.select_one("td > div.L_MyAd_").text
-      title = job.select_one("td > a > span.company").text
-      part_time = job.select_one("td > span.time").text
-      money_type = job.select_one("td > span.hour").text 
-      pay = job.select_one("td > span.number").text
-      post_time = job.select_one("td.regDate > strong").text
-
-      print(title)
-
+      location = job.select_one("td.local").text
+      title = job.select_one("td.title > a > span.company").text
+      part_time = job.select_one("td.data > span").text   
+      pay = ''.join([x.text for x in job.find_all('span')])
+      post_time = job.select_one("td.regDate").text
       wr = csv.writer(f)
-      wr.writerow([location, title, part_time, money_type+pay, post_time])
+      wr.writerow([location, title, part_time, pay, post_time])
 
     f.close()
+  
